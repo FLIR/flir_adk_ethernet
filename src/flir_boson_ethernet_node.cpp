4a9fd36a7a1855ae693ac88c240c5e5c80da4335
Anil Dhurjaty <anil.dhurjaty@flir.com>
2019-06-13 16:01:01 -0700

    adds image nodelet to c++ node

--- src/flir_boson_ethernet_node.cpp
+++ src/flir_boson_ethernet_node.cpp
@@ -5,6 +5,8 @@
 #include <nodelet/loader.h>
 #include <ros/console.h>
 
+using namespace std;
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "Boson Camera Node");
@@ -12,7 +14,13 @@ int main(int argc, char** argv)
   nodelet::M_string remap(ros::names::getRemappings());
   nodelet::V_string nargv;
   std::string nodelet_name = ros::this_node::getName();
+  // for(ros::M_string::const_iterator it = remap.cbegin(); it != remap.cend(); ++it)
+  // {
+  //     ROS_INFO("%s %s", it->first.c_str(), it->second.c_str());
+  // }
   nodelet.load(nodelet_name, "flir_boson_ethernet/BosonCamera", remap, nargv);
-  ros::spin();
+  nodelet.load("image_view", "image_view/image", remap, nargv);
+  ros::MultiThreadedSpinner spinner(2);
+  spinner.spin();
   return 0;
 }
