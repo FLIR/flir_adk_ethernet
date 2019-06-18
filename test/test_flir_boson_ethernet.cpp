50e2342ce2f17cbcf63c57b71dd7ee96e018dc91
Anil Dhurjaty <anil.dhurjaty@flir.com>
2019-06-18 15:44:21 -0700

    small tweaks

--- test/test_flir_boson_ethernet.cpp
+++ test/test_flir_boson_ethernet.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 
+// ImageEvent tests
+// TEST(ImageEventTests, )
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv); 
     return RUN_ALL_TESTS();
