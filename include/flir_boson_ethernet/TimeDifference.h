51503c204e0bc8fc36298b8a3c84cd4ea8b38f83
Anil Dhurjaty <anil.dhurjaty@flir.com>
2019-06-20 09:50:33 -0700

    fills out time differences class

--- include/flir_boson_ethernet/TimeDifference.h
+++ include/flir_boson_ethernet/TimeDifference.h
@@ -1,7 +1,15 @@
+// C++ Includes
+#include <iostream>
+#include <vector>
+#include <numeric>
+
 // ROS Includes
 #include <ros/ros.h>
 #include <nodelet/nodelet.h>
 #include <std_msgs/Header.h>
+#include <sensor_msgs/Image.h>
+
+#define NUM_READINGS 60
 
 using namespace std;
 
@@ -15,10 +23,18 @@ class TimeDifference : public nodelet::Nodelet {
 
   private:
     virtual void onInit();
-    void calculateDifferences(const std_msgs::Header::ConstPtr& msg);
+    void calculateDifferences();
+    void getImageHeader(const sensor_msgs::ImageConstPtr& msg);
+    void getActualTimeHeader(const std_msgs::Header::ConstPtr& msg);
+    void addTimeDiff();
 
     ros::NodeHandle _nh, _pnh;
-    ros::Subscriber _subscriber;
+    ros::Subscriber _imgSubscriber;
+    ros::Subscriber _actualTimeSubscriber;
+    std_msgs::Header _imgHeader;
+    std_msgs::Header _actualTimeHeader;
+
+    std::vector<uint32_t> _timeDifferences;
 };
 
 }
--- src/nodelets/TimeDifference.cpp
+++ src/nodelets/TimeDifference.cpp
@@ -3,7 +3,7 @@
 using namespace flir_boson_ethernet;
 
 TimeDifference::TimeDifference() {
-
+    
 }
 
 TimeDifference::~TimeDifference() {
@@ -11,9 +11,44 @@ TimeDifference::~TimeDifference() {
 }
 
 void TimeDifference::onInit() {
+    _nh = getNodeHandle();
+    _pnh = getPrivateNodeHandle();
 
+    _imgSubscriber = _nh.subscribe<sensor_msgs::ImageConstPtr>("image_raw", 1,
+        boost::bind(&TimeDifference::getImageHeader, this, _1));
+    _actualTimeSubscriber = _nh.subscribe<std_msgs::Header>("actual_timestamp", 1,
+        boost::bind(&TimeDifference::getImageHeader, this, _1));
 }
 
-void TimeDifference::calculateDifferences(const std_msgs::Header::ConstPtr& msg) {
-    
+void TimeDifference::calculateDifferences() {
+    auto sum = accumulate(_timeDifferences.begin(), _timeDifferences.end(), 0.0);
+    auto avg = sum / (double)_timeDifferences.size();
+    auto msAvg = avg / 1e6;
+
+    std::cout << getName() << " - Avg timestamp difference: " << msAvg << " ms" << std::endl;
+}
+
+void TimeDifference::getImageHeader(const sensor_msgs::ImageConstPtr& img) {
+    _imgHeader = img->header;
+    if(_imgHeader.seq == _actualTimeHeader.seq) {
+        addTimeDiff();
+    }
+}
+
+void TimeDifference::getActualTimeHeader(const std_msgs::Header::ConstPtr& msg) {
+    _actualTimeHeader = *msg;
+    if(_imgHeader.seq == _actualTimeHeader.seq) {
+        addTimeDiff();
+    }
+}
+
+void TimeDifference::addTimeDiff() {
+    auto t1 = _imgHeader.stamp.nsec;
+    auto t2 = _actualTimeHeader.stamp.nsec;
+    auto diff = abs(t1 - t2);
+    _timeDifferences.push_back((uint32_t)diff);
+
+    if(_timeDifferences.size() >= NUM_READINGS) {
+        calculateDifferences();
+    }
 }
